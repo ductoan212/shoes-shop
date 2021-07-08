9474a86ec831eb74324747f24f9fcff066e44ca8
Duc Toan <ductoan20102000@gmail.com>
2021-07-08 18:58:07 +0700

    cập nhật link mongdodb

--- src/index.js
+++ src/index.js
@@ -40,8 +40,7 @@ app.use(fileUpload());
 
 // connect to mongodb cloud
 mongoose.connect(
-  // process.env.MONGODB_URI ||
-  'mongodb://localhost:27017/shoe_shop',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/shoe_shop',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
