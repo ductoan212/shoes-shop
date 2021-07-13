f71c2d17552a704050d14ec1b82c59076042a440
DO QUOC VINH <18521645@ms.uit.edu.vn>
2021-07-13 22:19:53 +0700

    profile và những thứ liên quan

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,111 +2,113 @@ var express = require('express');
 var User = require('../models/userModel.js');
 var { isAdmin, isLogin } = require('../utils.js');
 var data = require('../data.js');
+const { response } = require('express');
 
 const userRouter = express.Router();
 
-userRouter.get('/', isAdmin, async (req, res) => {
-  const pageSize = 3;
-  const page = Number(req.query.pageNumber) || 1;
-  const count = await User.countDocuments({});
-  const users = await User.find({})
-    .skip(pageSize * (page - 1))
-    .limit(pageSize);
+userRouter.get('/', isAdmin, async(req, res) => {
+    const pageSize = 3;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await User.countDocuments({});
+    const users = await User.find({})
+        .skip(pageSize * (page - 1))
+        .limit(pageSize);
 
-  const isLogin = req.session.user ? true : false;
-  const user = req.session.user ? req.session.user : {};
-  res.render('userListAdmin', {
-    isLogin,
-    user,
-    users,
-    page,
-    pages: Math.ceil(count / pageSize),
-  });
+    const isLogin = req.session.user ? true : false;
+    const user = req.session.user ? req.session.user : {};
+    res.render('userListAdmin', {
+        isLogin,
+        user,
+        users,
+        page,
+        pages: Math.ceil(count / pageSize),
+    });
 });
 
-userRouter.get('/seed', async (req, res) => {
-  await User.remove({});
-  const createUsers = await User.insertMany(data.users);
-  res.send({ createUsers });
+userRouter.get('/seed', async(req, res) => {
+    await User.remove({});
+    const createUsers = await User.insertMany(data.users);
+    res.send({ createUsers });
 });
 
-userRouter.get('/login', async (req, res) => {
-  if (req.session.user) {
-    res.redirect('/');
-  }
-  let error =
-    req.session.error && req.session.error.login ? req.session.error.login : '';
-  req.session.error = {};
-  res.render('login', { error });
+userRouter.get('/login', async(req, res) => {
+    if (req.session.user) {
+        res.redirect('/');
+    }
+    let error =
+        req.session.error && req.session.error.login ? req.session.error.login : '';
+    req.session.error = {};
+    res.render('login', { error });
 });
 
-userRouter.get('/register', async (req, res) => {
-  let error =
-    req.session.error && req.session.error.register
-      ? req.session.error.register
-      : '';
-  req.session.error = {};
-  res.render('register', { error });
+userRouter.get('/register', async(req, res) => {
+    let error =
+        req.session.error && req.session.error.register ?
+        req.session.error.register :
+        '';
+    req.session.error = {};
+    res.render('register', { error });
 });
 
-userRouter.post('/login', async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user) {
-    if (user.password === req.body.password) {
-      req.session.user = {
+userRouter.post('/login', async(req, res) => {
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+        if (user.password === req.body.password) {
+            req.session.user = {
+                _id: user._id,
+                username: user.username,
+                fullname: user.fullname,
+                email: user.email,
+                address: user.address,
+                phonenumber: user.phonenumber,
+                isAdmin: user.isAdmin,
+            };
+            res.redirect('/');
+            return;
+        }
+    }
+    req.session.error = { login: 'Invalid username or password' };
+    res.redirect('/user/login');
+});
+
+userRouter.post('/register', async(req, res) => {
+    const userTemp = await User.findOne({ username: req.body.username });
+    if (userTemp) {
+        req.session.error = { register: 'Username is exists' };
+        res.redirect('/user/register');
+        return;
+    }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        fullname: req.body.fullname,
+        email: req.body.email,
+        phonenumber: req.body.phonenumber,
+        address: req.body.address,
+    });
+    // await User.remove({});
+    const createUser = await user.save();
+    req.session.user = {
         _id: user._id,
         username: user.username,
-        fullname: user.fullname,
+        fullname: user.name,
         email: user.email,
         address: user.address,
         phonenumber: user.phonenumber,
         isAdmin: user.isAdmin,
-      };
-      res.redirect('/');
-      return;
-    }
-  }
-  req.session.error = { login: 'Invalid username or password' };
-  res.redirect('/user/login');
-});
-
-userRouter.post('/register', async (req, res) => {
-  const userTemp = await User.findOne({ username: req.body.username });
-  if (userTemp) {
-    req.session.error = { register: 'Username is exists' };
-    res.redirect('/user/register');
-    return;
-  }
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-    fullname: req.body.fullname,
-    email: req.body.email,
-    phonenumber: req.body.phonenumber,
-    address: req.body.address,
-  });
-  // await User.remove({});
-  const createUser = await user.save();
-  req.session.user = {
-    _id: user._id,
-    username: user.username,
-    fullname: user.name,
-    email: user.email,
-    address: user.address,
-    phonenumber: user.phonenumber,
-    isAdmin: user.isAdmin,
-  };
-  res.redirect('/');
+    };
+    res.redirect('/');
 });
 
 userRouter.get('/logout', (req, res) => {
-  req.session.user = null;
-  res.redirect('/');
+    req.session.user = null;
+    res.redirect('/');
 });
 
-userRouter.get('/profile', isLogin, async (req, res) => {
-  const user = req.session.user;
-  res.json({ user });
+userRouter.get('/profile', isLogin, async(req, res) => {
+    const isLogin = req.session.user ? true : false;
+    const user = req.session.user;
+    res.render('profile', { user, isLogin });
 });
 
 module.exports = userRouter;
